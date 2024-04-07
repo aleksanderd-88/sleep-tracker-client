c0ff3a2df19ce72fcaab2f339b0274f7e751764f
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-04-07 22:06:33 +0200

    Add start view content

---
+++ src/common/components/AppButton.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import type { ButtonHTMLAttributes, PropType } from 'vue';
+
+  defineProps({
+    type: {
+      type: String as PropType<ButtonHTMLAttributes['type']>,
+      default: 'button'
+    }
+  })
+</script>
+
+<template>
+  <button :type="type" class="app-btn">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  .app-btn {
+    cursor: pointer;
+    outline: none;
+    border: none;
+  }
+</style>
--- src/pages/StartView.vue
+++ src/pages/StartView.vue
@@ -1,9 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppButton from '@/common/components/AppButton.vue';
+</script>
 
 <template>
-  <div class="base-layout">
-    <main class="base-layouts__content">
-      <h1>Daily Sleep Tracker</h1>
+  <div class="start-view base-layout">
+    <main class="start-view__content base-layouts__content">
+      <h1 class="start-view__heading">Daily Sleep <span>Tracker</span></h1>
+      <AppButton class="start-view__action-btn">Track your sleep today!</AppButton>
     </main>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .start-view {
+    display: flex;
+
+    &__content {
+      margin: auto;
+    }
+
+    &__heading {
+      text-align: center;
+      font-weight: 700;
+      font-size: clamp(3rem, 8vw, 7rem);
+
+      span {
+        display: block;
+      }
+    }
+
+    &__action-btn {
+      width: 100%;
+      height: 60px;
+      border-radius: 15px;
+      background-color: $light;
+      font-size: 1.25rem;
+      font-weight: 500;
+      color: $dark;
+      margin: 4rem 0 0;
+    }
+  }
+</style>
