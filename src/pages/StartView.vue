d3ef0f6853b1e71ba14b90a94be7e916e022ae12
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-04-07 21:42:06 +0200

    Implement base layout class

--- src/pages/StartView.vue
+++ src/pages/StartView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Daily Sleep Tracker</h1>
+  <div class="base-layout">
+    <main class="base-layouts__content">
+      <h1>Daily Sleep Tracker</h1>
+    </main>
   </div>
 </template>
